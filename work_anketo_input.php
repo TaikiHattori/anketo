ef4e6b91bdc43bece6b18f08e59ed20f2ddba4e3
TaikiHattori <tvajarvis@outlook.jp>
2025-02-02 17:31:00 +0900

    コード整頓＆方向性決め

--- work_anketo_input.php
+++ work_anketo_input.php
@@ -4,25 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ストリートダンスリスト（入力画面）</title>
+    <title>筋トレ（入力画面）</title>
 </head>
 
 <body>
     <form action="work_anketo_create.php" method="POST">
         <fieldset>
-            <legend>ストリートダンスリスト（入力画面）</legend>
-            <a href="work_anketo_read.php">一覧画面</a>
+            <legend>筋トレ（入力画面）</legend>
+
             <div>
-                ジャンル名: <input type="text" name="janru">
+                筋トレ日: <input type="date" name="tanjo">
             </div>
             <div>
-                誕生年: <input type="date" name="tanjo">
+                部位: <input type="text" name="janru">
             </div>
+
             <div>
-                <button>submit</button>
+                <button>登録</button>
             </div>
         </fieldset>
     </form>
+
+    <a href="work_anketo_read.php">一覧画面へ</a>
 
 </body>
 
